5365ad0e03d624ce2e84a1f9f4fe065e5281ef9c
Artem <artem.taturevych@xarial.com>
2020-03-10 09:42:05 +1100

    Added missing files

---
+++ events-handler/vb/AddIn.vb
@@ -0,0 +1,11 @@
+﻿Imports System.ComponentModel
+
+<Runtime.InteropServices.ComVisible(True)>
+<DisplayName("Event Handlers Example")>
+Public Class AddIn
+    Inherits Xarial.XCad.SolidWorks.SwAddInEx
+
+    Public Overrides Sub OnConnect()
+        Application.Documents.RegisterHandler(Of DocHandler)()
+    End Sub
+End Class
