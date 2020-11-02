a7401d730bd1fffbf9ac9acc78f765032dc7242d
Artem <artem.taturevych@xarial.com>
2020-11-02 15:12:52 +1100

    Removed unused namespaces

--- SweepSketch/cs/SweepSketchAddIn.cs
+++ SweepSketch/cs/SweepSketchAddIn.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using Xarial.XCad.Base.Attributes;
 using Xarial.XCad.Features.CustomFeature.Delegates;
 using Xarial.XCad.Geometry;
