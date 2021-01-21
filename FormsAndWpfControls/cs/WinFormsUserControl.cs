a04d42be4869b925f702d18fc8ce3ac5d3882d44
Artem <artem.taturevych@xarial.com>
2021-01-22 09:38:00 +1100

    Moved the project, added icons

--- FormsAndWpfControls/cs/WinFormsUserControl.cs
+++ FormsAndWpfControls/cs/WinFormsUserControl.cs
@@ -7,9 +7,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Xarial.XCad.Base.Attributes;
+using FormsAndWpfControls.Properties;
 
 namespace FormsAndWpfControls
 {
+    [Icon(typeof(Resources), nameof(Resources.winforms_icon))]
     public partial class WinFormsUserControl : UserControl
     {
         public WinFormsUserControl()
---
+++ FormsAndWpfControls/cs/WpfUserControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using FormsAndWpfControls.Properties;
+using System.Windows.Controls;
+using Xarial.XCad.Base.Attributes;
+
+namespace FormsAndWpfControls
+{
+    [Icon(typeof(Resources), nameof(Properties.Resources.wpf_icon))]
+    public partial class WpfUserControl : UserControl
+    {
+        public WpfUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
