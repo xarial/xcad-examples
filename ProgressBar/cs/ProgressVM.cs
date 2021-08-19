11a6e92ba9be16ea8c1e0f68e2581c72a1d9baf1
Artem <artem.taturevych@xarial.com>
2021-08-19 14:27:17 +1000

    Added progress bar example

---
+++ ProgressBar/cs/ProgressVM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xarial.XCad.Examples.ProgressBar
+{
+    public class ProgressVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private double m_Progress;
+
+        public double Progress
+        {
+            get => m_Progress;
+            set 
+            {
+                m_Progress = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Progress)));
+            }
+        }
+    }
+}
