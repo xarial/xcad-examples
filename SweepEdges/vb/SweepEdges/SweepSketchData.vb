1661053fca26bc08c40b522e065ea5abaccbc51c
Artem <artem.taturevych@xarial.com>
2022-01-20 20:55:05 +1100

    Added sweep edges VB.NET example

---
+++ SweepEdges/vb/SweepEdges/SweepSketchData.vb
@@ -0,0 +1,23 @@
+﻿Imports System.Runtime.InteropServices
+Imports Xarial.XCad.Base.Attributes
+Imports Xarial.XCad.Features.CustomFeature.Attributes
+Imports Xarial.XCad.Features.CustomFeature.Enums
+Imports Xarial.XCad.SolidWorks.Geometry
+Imports Xarial.XCad.SolidWorks.UI.PropertyPage
+Imports Xarial.XCad.UI.PropertyPage.Attributes
+Imports Xarial.XCad.UI.PropertyPage.Enums
+
+<ComVisible(True)>
+<Title("Swept Edges")>
+Public Class SweepSketchData
+    Inherits SwPropertyManagerPageHandler
+
+    <ControlOptions(AddControlOptions_e.Visible Or AddControlOptions_e.Enabled, ControlLeftAlign_e.LeftEdge, 0, 0, -1, -1, -1, 50)>
+    Public Property Edges As List(Of ISwLinearEdge)
+
+    <NumberBoxOptions(NumberBoxUnitType_e.Length, 0.000001, 1000, 0.01, True, 0.02, 0.001)>
+    <StandardControlIcon(BitmapLabelType_e.Radius)>
+    <ParameterDimension(CustomFeatureDimensionType_e.Radial)>
+    Public Property Radius As Double = 0.005
+
+End Class
---
+++ SweepEdges/vb/SweepEdges/SweepSketchMacroFeatureEditor.vb
@@ -0,0 +1,58 @@
+﻿
+Imports System.Runtime.InteropServices
+Imports Xarial.XCad.Base.Attributes
+Imports Xarial.XCad.Features.CustomFeature
+Imports Xarial.XCad.Features.CustomFeature.Delegates
+Imports Xarial.XCad.Geometry
+Imports Xarial.XCad.Geometry.Structures
+Imports Xarial.XCad.SolidWorks
+Imports Xarial.XCad.SolidWorks.Documents
+Imports Xarial.XCad.SolidWorks.Features.CustomFeature
+Imports Xarial.XCad.SolidWorks.Geometry
+
+<ComVisible(True)>
+<Title("Swept Edges")>
+Public Class SweepSketchMacroFeatureEditor
+    Inherits SwMacroFeatureDefinition(Of SweepSketchData, SweepSketchData)
+
+    Public Overrides Function CreateGeometry(ByVal app As ISwApplication, ByVal model As ISwDocument, ByVal data As SweepSketchData, ByVal isPreview As Boolean, <Out> ByRef alignDim As AlignDimensionDelegate(Of SweepSketchData)) As ISwBody()
+
+        Dim result = New List(Of ISwBody)()
+        Dim firstCenterPt As Point = Nothing
+        Dim firstDir As Vector = Nothing
+
+        For Each edge In data.Edges
+
+            Dim path = edge.Definition
+            Dim startPt = path.StartCoordinate
+            Dim endPt = path.EndCoordinate
+            Dim dir = startPt - endPt
+
+            If firstCenterPt Is Nothing Then
+                firstCenterPt = startPt
+            End If
+
+            If firstDir Is Nothing Then
+                firstDir = dir
+            End If
+
+            Dim profile = app.MemoryGeometryBuilder.CreateCircle(startPt, dir, data.Radius * 2)
+            Dim profileRegion = app.MemoryGeometryBuilder.CreateRegionFromSegments(profile)
+            Dim region = app.MemoryGeometryBuilder.CreatePlanarSheet(profileRegion).Bodies.First()
+            Dim sweep = app.MemoryGeometryBuilder.CreateSolidExtrusion(path.Length, dir, region)
+            Dim body = sweep.Bodies.OfType(Of ISwBody)().First()
+
+            result.Add(body)
+        Next
+
+        alignDim = Sub(name, [dim])
+                       Select Case name
+                           Case NameOf(SweepSketchData.Radius)
+                               Me.AlignRadialDimension([dim], firstCenterPt, firstDir)
+                       End Select
+                   End Sub
+
+        Return result.ToArray()
+    End Function
+
+End Class
